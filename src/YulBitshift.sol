8d2cce7e2cda1b360ba0d1a8ccd33080f2880c5b
brandon engstrom <bengstrom32@gmail.com>
2023-10-26 15:29:49 -0400

    first commit

--- src/YulBitshift.sol
+++ src/YulBitshift.sol
@@ -5,8 +5,21 @@ pragma solidity 0.8.17;
 
 contract YulBitShift  {
 
+    uint128 public C = 4;
+    uint96 public D = 6;
+    uint16 public E = 8;
+    uint8 public F = 1;
+
+    function readyBySlot(uint256 slot) external view returns(bytes32 value) {
+        assembly {
+            value:= sload(slot)
+        }
+    }
    
-    
+    // NOT FOR USE IN PRODUCTION
+    function writeBySlot(uint256 slot, uint256 value) external {
+        
+    }
 
 
 }
