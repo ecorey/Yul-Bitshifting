a925294de2dbf33169e7a958ff3cf5c207f0b48f
brandon engstrom <bengstrom32@gmail.com>
2023-10-26 15:52:22 -0400

    first commit

--- src/YulBitshift.sol
+++ src/YulBitshift.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.17;
 
+// getting variable E in slot 0 as it is packed with others
 
 contract YulBitShift  {
 
@@ -10,6 +11,7 @@ contract YulBitShift  {
     uint16 public E = 8;
     uint8 public F = 1;
 
+    // e.g. 0
     function readyBySlot(uint256 slot) external view returns(bytes32 value) {
         assembly {
             value:= sload(slot)
@@ -18,7 +20,41 @@ contract YulBitShift  {
    
     // NOT FOR USE IN PRODUCTION
     function writeBySlot(uint256 slot, uint256 value) external {
+
+    }
+
+    function getOffsetE() external pure returns (uint256 slot, uint256 offset) {
+        assembly {
+            slot := E.slot
+            offset := E.offset
+        }
+    }
+
+    function readE() external view returns(uint e) {
+        assembly {
+            let value := sload(E.slot) // loads 32 byte increaments
+            // 0x0000000000000000000000000000000000000000000000000000000000010008
+            // E.offset = 28
+            let shifted := shr(mul(E.offset, 8), value)
+
+            // eq to:
+            // 0x00000000000000000000000000000000000000000000000000000000ffffffff
+            e:= and(0xffffffff, shifted)
+
+        }
+    }
+
+    function readEAlt() external view returns(uint256 e) {
         
+        assembly{
+            let slot := sload(E.slot)
+            let offset := sload(E.offset)
+            let value := sload(E.slot)
+
+            let shifted := div(value, 0x1000000000000000000000000000000000000000000000000000000000)
+            e := and(0xffffffff, shifted)
+        }
+
     }
 
 
